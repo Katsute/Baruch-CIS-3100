e65b0d2e29bd1d08074a1df2cf4f566848c8a3e1
Katsute <58778985+Katsute@users.noreply.github.com>
2021-02-08 14:12:52 -0500

    m

---
+++ classwork/02_08_2021/02_08_2021.cpp
@@ -0,0 +1,25 @@
+#include <iostream> // import lib/file
+using namespace std; // static import
+
+// generally follows Java coding conventions.
+int main()
+{
+    /*
+        same data types as Java
+        same naming conditions as Java
+    */
+    double hours, rate, pay;
+
+    // << insert into stream (sys.out)
+    cout << "How many hours did you work? "; // sys out
+    // >> write from steam (sys.in)
+    cin >> hours; // sys in, auto parses num
+
+    cout << "How much do you get paid per hour? "; // sys out
+    cin >> rate; // sys in
+
+    pay = hours * rate;
+
+    cout << "You have earned $" << pay << endl; // str concat + endline
+    return 0;
+}
