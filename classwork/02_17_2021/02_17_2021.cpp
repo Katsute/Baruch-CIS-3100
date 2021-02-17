92ae9cc2bbb23c1e3ef5b2d99e247daba92b0045
Katsute <58778985+Katsute@users.noreply.github.com>
2021-02-17 14:04:57 -0500

    sys.in

---
+++ classwork/02_17_2021/02_17_2021.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length, width, area;
+
+    cout << "This program calculates the area of a ";
+    cout << "rectangle.\n";
+    cout << "What is the length and width of the rectangle? ";
+    cin >> length >> width; // input space/newline separated
+    // cout << "What is the length of the rectangle? ";
+    // cin >> length;
+    // cout << "What is the width of the rectangle? ";
+    // cin >> width;
+    area = length * width;
+    cout << "The area of the rectangle is " << area << ".\n";
+    return 0;
+}
