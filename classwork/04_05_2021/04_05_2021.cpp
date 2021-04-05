824db31d25383400fb6cd38a52b1c272c8d9fe3d
Katsute <58778985+Katsute@users.noreply.github.com>
2021-04-05 14:06:01 -0400

    linear/binary search

---
+++ classwork/04_05_2021/04_05_2021.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int[], int, int);
+int binarySearch(int[], int, int);
+
+int main(){
+
+}
+
+// linear search -> check each element until match
+int linearSearch(int arr[], int size, int value){
+    int index = 0;
+    int position = -1;
+    bool found = false;
+    while(index < size && !found){
+        if(arr[index] == value){
+            found = true;
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+// binary search -> divide in half and check left and write until match
+// https://www.youtube.com/watch?v=KXJSjte_OAI
+int binarySearch(int orderedArr[], int size, int value){
+    int first = 0;
+    int last = size;
+    int position = -1;
+    bool found = false;
+    while(first < last && !found){
+        int middle = (last - first) / 2;
+        if(orderedArr[middle] == value){
+            found = true;
+            position = middle;
+        }else if(orderedArr[middle] > value){
+            last = middle - 1;
+        }else{
+            first = middle + 1;
+        }
+    }
+    return position;
+}
